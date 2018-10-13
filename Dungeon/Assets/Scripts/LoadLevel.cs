d86bb3d518bffa73b5d5cc73dafd04cc08ac7ab9
StephenB <43865924+Quarpion@users.noreply.github.com>
2018-10-12 22:36:01 -0400

    Removed some files.

--- Dungeon/Assets/Scripts/LoadLevel.cs
+++ Dungeon/Assets/Scripts/LoadLevel.cs
@@ -6,9 +6,19 @@ using UnityEngine.SceneManagement;
 public class LoadLevel : MonoBehaviour {
 
     public string LeveltoLoad;
+    public Vector2 position;
 
     public void StartGame()
     {
         SceneManager.LoadScene(LeveltoLoad);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(LeveltoLoad);
+            other.SendMessage("ChangePosition", position, SendMessageOptions.DontRequireReceiver);
+        }
     }
 }
