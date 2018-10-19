96d97a6fc8004e5381499dd2121e8cc6ed6c6ffe
ElijahBelov <44126195+ElijahBelov@users.noreply.github.com>
2018-10-19 00:32:40 -0400

    fixed bomb enemy playerdetector collider and player shots not being destroyed

---
+++ Dungeon/Assets/Scripts/Map/LevelboundsTagger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelboundsTagger : MonoBehaviour {
+
+	void Start () {
+        foreach (Transform t in transform)
+        {
+            t.gameObject.tag = "Levelbounds";
+        }
+        gameObject.tag = "Levelbounds";
+    }
+}
